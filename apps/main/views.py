72eab364f0cde7a2ce5da653bf56aa953f7f4653
Apanasenko <alexander.apanasenko@gmail.com>
2015-02-27 22:46:57 +1000

    Добавлено отображение всех турниров на главную страницу

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from apps.tournament.models import Tournament
 
 
 def index(request):
-    return render(request, 'main/base.html', {})
+    return render(request, 'main/base.html', {'tournaments': Tournament.objects.all()})
