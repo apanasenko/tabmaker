fd008dd81cd5fea48f5ca888a16ea2de1f778806
Apanasenko <alexander.apanasenko@gmail.com>
2015-03-04 13:40:05 +1000

    Перевод времени в UTC формат при сохранении турнира

--- apps/tournament/models.py
+++ apps/tournament/models.py
@@ -4,6 +4,8 @@ from apps.game.models import Game
 from apps.motion.models import Motion
 from apps.team.models import Team
 
+import pytz
+
 
 class Tournament(models.Model):
     name = models.CharField(max_length=100)
@@ -16,6 +18,12 @@ class Tournament(models.Model):
     user_members = models.ManyToManyField(User, through='UserTournamentRel')
     team_members = models.ManyToManyField(Team, through='TeamTournamentRel')
     info = models.TextField()
+
+    def save(self, *args, **kwargs):
+        self.open_reg = self.open_reg.astimezone(pytz.utc)
+        self.close_reg = self.close_reg.astimezone(pytz.utc)
+        self.start_tour = self.start_tour.astimezone(pytz.utc)
+        super(Tournament, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.name
