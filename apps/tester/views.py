590888f3425772d94ad59f61b05f180c948c4b60
Apanasenko <alexander.apanasenko@gmail.com>
2015-03-28 01:06:59 +1000

    Изменены имена тестовых юзеров и команд

--- apps/tester/views.py
+++ apps/tester/views.py
@@ -12,16 +12,16 @@ import random
 def generate_user(name_length=5):
     letters = list(map(chr, range(ord('a'), ord('z') + 1)))
 
-    name = ''
+    name = 'user_'
     while name == '':
         for i in range(name_length):
             name += random.choice(letters)
         if len(User.objects.filter(username=name)):
             name_length += 1
-            name = ''
+            name = 'user_'
 
     password = '1'
-    email = name + '@aaa.aaa'
+    email = name + '@test.test'
     u = User.objects.create_user(username=name, email=email, password=password)
     u.save()
     e = EmailAddress.objects.create(user=u, email=u.email, verified=True, primary=True)
@@ -33,14 +33,14 @@ def generate_user(name_length=5):
 def generate_team(name_length=5):
     letters = list(map(chr, range(ord('a'), ord('z') + 1)))
 
-    name = ''
+    name = 'test_team_'
     while name == '':
         for i in range(name_length):
             name += random.choice(letters)
         print(name_length, name, Team.objects.filter(name=name))
         if len(Team.objects.filter(name=name)):
             name_length += 1
-            name = ''
+            name = 'test_team_'
 
     t = Team.objects.create(
         name=name,
