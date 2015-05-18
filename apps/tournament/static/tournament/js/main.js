09502c6008cf1b9cf0ee8b0557462eb5a996db5b
Apanasenko <alexander.apanasenko@gmail.com>
2015-05-19 00:39:14 +1000

    Fix: добавил js для перемещения блоков
    Не заметил, что папка static была заигнорена

---
+++ apps/tournament/static/tournament/js/main.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Alexander on 22.04.2015.
+ */
+
+$(document).ready(function() {
+    $('select').material_select();
+});
---
+++ apps/tournament/static/tournament/js/swap_block.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Alexander on 17.05.2015.
+ */
+
+function swap(element, buffer, element_name){
+    if (!buffer){
+        $(element).css('background', '#fff');
+        return element
+    } else {
+        var class_id = '.' + element_name + '_id';
+        var class_name = '.' + element_name + '_name'
+        $(buffer).css('background', $(element).parent().css('background'));
+
+        var team_name = $(element).find(class_name).html();
+        var team_id = $(element).find(class_id).val();
+
+        $(element).find(class_name).html($(buffer).find(class_name).html());
+        $(buffer).find(class_name).html(team_name);
+
+        $(element).find(class_id).val($(buffer).find(class_id).val());
+        $(buffer).find(class_id).val(team_id);
+
+        return null;
+    }
+
+}
+
+$(document).ready(function() {
+    var buffer_team = null;
+    var buffer_chair = null;
+
+    $('.team').click(function(){
+        buffer_team = swap(this, buffer_team, 'team');
+    });
+
+    $('.chair').click(function(){
+        buffer_chair = swap(this, buffer_chair, 'chair');
+    });
+});
