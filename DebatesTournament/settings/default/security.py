58070dd77581960f71a145deaf2384ae5961d6b2
Alexander Apanasenko <alexander.apanasenko@gmail.com>
2015-09-04 18:45:28 +1000

    Файлы настроек по умолчанию сохранены в UTF-8. Параметры для настройки выделены '<>'

--- DebatesTournament/settings/default/security.py
+++ DebatesTournament/settings/default/security.py
@@ -1,5 +1,5 @@
-﻿# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'SECRET_KEY'
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '<SECRET_KEY>' # Set random line
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
