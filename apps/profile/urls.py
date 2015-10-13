2486304fb18957ef4a9620eb9d92771a73836eb2
Alexander Apanasenko <alexander.apanasenko@gmail.com>
2015-10-14 03:12:47 +1000

    Добавлена функция востановления пароля

--- apps/profile/urls.py
+++ apps/profile/urls.py
@@ -15,6 +15,17 @@ urlpatterns = patterns(
 
     url(r"^confirm-email/$", allauth_views.email_verification_sent, name="account_email_verification_sent"),
     url(r"^confirm-email/(?P<key>\w+)/$", allauth_views.confirm_email, name="account_confirm_email"),
-    # Handle old redirects
-    url(r"^confirm_email/(?P<key>\w+)/$", RedirectView.as_view(url='/accounts/confirm-email/%(key)s/', permanent=True)),
+
+    url(r"^password/reset/$", allauth_views.password_reset, name="account_reset_password"),
+    url(r"^password/reset/done/$", allauth_views.password_reset_done, name="account_reset_password_done"),
+    url(
+        r"^password/reset/key/(?P<uidb36>[0-9A-Za-z]+)-(?P<key>.+)/$",
+        allauth_views.password_reset_from_key,
+        name="account_reset_password_from_key"
+    ),
+    url(
+        r"^password/reset/key/done/$",
+        allauth_views.password_reset_from_key_done,
+        name="account_reset_password_from_key_done"
+    ),
 )
