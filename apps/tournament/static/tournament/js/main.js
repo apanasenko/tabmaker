c98ee2a8ec0cb597e237c70c12cc7938bc0d7b90
Alexander Apanasenko <alexander.apanasenko@gmail.com>
2015-10-15 13:32:20 +1000

    Разделил tourmaent/main.js на несколько файлов

--- apps/tournament/static/tournament/js/main.js
+++ apps/tournament/static/tournament/js/main.js
@@ -1,37 +1,3 @@
-function csrfSafeMethod(method) {
-    // these HTTP methods do not require CSRF protection
-    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
-}
-
-
-function getCookie(name) {
-    var cookieValue = null;
-    if (document.cookie && document.cookie != '') {
-        var cookies = document.cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            var cookie = jQuery.trim(cookies[i]);
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) == (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-
-
-$(document).ready(function() {
-    $.ajaxSetup({
-        beforeSend: function(xhr, settings) {
-            if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
-                xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
-            }
-        }
-    });
-});
-
-
 function remove_team(url, id){
     $.post(url, {id: id},
         function(data, status){
