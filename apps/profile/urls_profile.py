c28f489cf17db2a209552b2b9cf141d61be8653b
Alexander Apanasenko <alexander.apanasenko@gmail.com>
2015-10-16 16:01:52 +1000

    Fix urls_profile: добавил символы конца строк для всех URL

--- apps/profile/urls_profile.py
+++ apps/profile/urls_profile.py
@@ -9,9 +9,9 @@ urlpatterns = patterns(
     "",
     url(r"^(?P<user_id>\d+)[/]$", views.show_profile, name='main'),
     url(r"^(?P<user_id>\d+)/edit[/]$", views.edit_profile, name='edit'),
-    url(r"^(?P<user_id>\d+)/tournaments[/]", views.show_tournaments_of_user, name='tournaments'),
-    url(r"^(?P<user_id>\d+)/teams[/]", views.show_teams_of_user, name='teams'),
-    url(r"^(?P<user_id>\d+)/adjudicator[/]", views.show_adjudicator_of_user, name='adjudicator'),
+    url(r"^(?P<user_id>\d+)/tournaments[/]$", views.show_tournaments_of_user, name='tournaments'),
+    url(r"^(?P<user_id>\d+)/teams[/]$", views.show_teams_of_user, name='teams'),
+    url(r"^(?P<user_id>\d+)/adjudicator[/]$", views.show_adjudicator_of_user, name='adjudicator'),
 
     url(r"^team/remove[/]$", utils.remove_team, name='team_remove'),
 )
