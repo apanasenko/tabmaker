3a240982ff68bf63f28aec8f48f4ebdaf4709180
Alexander Apanasenko <alexander.apanasenko@gmail.com>
2015-10-16 15:44:39 +1000

    tournament/main.js:remove_team переименовал параметр id -> team_id

--- apps/tournament/static/tournament/js/main.js
+++ apps/tournament/static/tournament/js/main.js
@@ -1,5 +1,5 @@
-    $.post(url, {id: id},
 function remove_team(url, id, div_team_id, div_errors_id){
+    $.post(url, {team_id: id},
         function(data, status){
             if (data.status == 'ok') {
                 $('#' + div_team_id).html(data.message)
--- apps/tournament/views.py
+++ apps/tournament/views.py
@@ -547,7 +547,7 @@ def edit_adjudicator_list(request, tournament):
 @access_by_status(name_page='')  # TODO  Добавить в таблицу доступа
 def remove_team(request, tournament):
     if request.method == 'POST' and request.is_ajax():
-        message = remove_team_from_tournament(tournament, request.POST['id'])
+        message = remove_team_from_tournament(tournament, request.POST['team_id'])
         return HttpResponse(json.dumps({'message': message}), content_type="application/json")
     raise Http404
 
