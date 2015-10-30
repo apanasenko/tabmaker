4449fc973142567c490f59c0a058523553185dd6
Alexander Apanasenko <alexander.apanasenko@gmail.com>
2015-10-31 00:37:01 +1000

    Добавлено выполнение миграции в update_script

--- project_update.sh
+++ project_update.sh
@@ -13,13 +13,25 @@ if [ "$answer" = "y" ]; then
       echo "Rebase conflicts!"
       exit
    fi
+fi
 
+echo -n "Would you like to do collect static (\"y\" or \"n\", default: \"n\"): "
+read answer
+if [ "$answer" = "y" ]; then
    source /home/ubuntu/tabmaker_env/bin/activate
    python manage.py collectstatic
    deactivate
 fi
 
-echo -n "Would you like restart server' (\"y\" or \"n\", default: \"n\"): "
+echo -n "Would you like to do migrate (\"y\" or \"n\", default: \"n\"): "
+read answer
+if [ "$answer" = "y" ]; then
+   source /home/ubuntu/tabmaker_env/bin/activate
+   python manage.py migrate
+   deactivate
+fi
+
+echo -n "Would you like restart server (\"y\" or \"n\", default: \"n\"): "
 read answer
 if [ "$answer" = "y" ]; then
    sudo service nginx restart
