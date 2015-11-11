5fea973dfe7ed7743d7410a97e0775223d3d34b3
Alexander Apanasenko <alexander.apanasenko@gmail.com>
2015-11-11 15:40:12 +1000

    Fix: перемещение блоков со спикерами

--- apps/tournament/static/tournament/js/reverse_speakers.js
+++ apps/tournament/static/tournament/js/reverse_speakers.js
@@ -1,9 +1,11 @@
 var swap_speakers = function(block){
     var s1 = $(block).parents('.team').find('.speaker_1');
     var s2 = $(block).parents('.team').find('.speaker_2');
-    var temp = s1.html();
-    s1.html(s2.html());
-    s2.html(temp);
+    if ($(block).is(':checked')){
+        s2.insertBefore(s1);
+    } else {
+        s1.insertBefore(s2);
+    }
 };
 
 
