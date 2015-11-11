79ac5c33cc1e9d98ad4e01fd6b0ca633075cf6f2
Alexander Apanasenko <alexander.apanasenko@gmail.com>
2015-11-11 15:44:07 +1000

    Проверка соотношения суммы спикерких на клиенте с учётом отсутствующих игроков

--- apps/tournament/static/tournament/js/check_result.js
+++ apps/tournament/static/tournament/js/check_result.js
@@ -10,10 +10,6 @@ $(document).ready(function(){
         if (!checkbox.length)
             return;
 
-function get_sum_speaker(block, position){
-    var team =$(block).find('.' + position);
-    return parseInt($(team).find('.speaker_1_points').val()) + parseInt($(team).find('.speaker_2_points').val())
-}
         var input = $(room).find('.is_check_game_input');
         $(checkbox).prop('checked', parseBool($(input).val()));
         $(checkbox).change(function(){
@@ -45,12 +41,31 @@ function check_results(){
 
         var checked_room = true;
         var errors_block = $(this).find('.errors');
+        var room = this;
         errors_block.html('');
-        var og = parseInt($(this).find('.og').find('.place').val());
-        var oo = parseInt($(this).find('.oo').find('.place').val());
-        var cg = parseInt($(this).find('.cg').find('.place').val());
-        var co = parseInt($(this).find('.co').find('.place').val());
-        var places = [og, oo, cg, co];
+        var results = {};
+        $.each(['og', 'oo', 'cg', 'co'], function(key, value){
+            var s1 = $(room).find('.' + value).find('.speaker_1');
+            var s2 = $(room).find('.' + value).find('.speaker_2');
+            results[value] = {
+                place: parseInt($(room).find('.' + value).find('.place').val()),
+                speaker_1: [
+                    $(s1).find('.exist_speaker').is(':checked'),
+                    parseInt($(s1).find('.speaker_1_points').val())
+                ],
+                speaker_2: [
+                    $(s2).find('.exist_speaker').is(':checked'),
+                    parseInt($(s2).find('.speaker_2_points').val())
+                ]
+            };
+
+            results[value].sum = results[value].speaker_1[1] + results[value].speaker_2[1];
+            results[value].is_all = results[value].speaker_1[0] && results[value].speaker_2[0];
+        });
+        var places = [];
+        $.each(results, function(key, value){
+            places.push(value.place)
+        });
         $.each([1, 2, 3, 4], function(key, value){
             if (places.indexOf(value) == -1){
                 errors_block.append('<p>Нет ' + value + ' места</p>');
@@ -62,18 +77,25 @@ function check_results(){
             }
         });
         if (checked_room){
-            var speakers_point = {};
-            speakers_point[og] = get_sum_speaker(this, 'og');
-            speakers_point[oo] = get_sum_speaker(this, 'oo');
-            speakers_point[cg] = get_sum_speaker(this, 'cg');
-            speakers_point[co] = get_sum_speaker(this, 'co');
+            var speakers = [];
+
+            $.each(results, function(key, value){
+                speakers[value.place] = value;
+            });
+
             $.each([4, 3, 2], function(key, value) {
 
-                if (speakers_point[value] > speakers_point[value - 1]) {
-                    errors_block.append(
-                        '<p>Сумма спикерских у ' + (value - 1).toString() + ' команды не должна быть больше чем у ' + value.toString() + ' команды</p>'
-                    );
-                    checked_room = false;
+                for(var i = value - 1; i > 0; i--){
+
+                    if(!speakers[value].is_all || !speakers[i].is_all)
+                        continue;
+
+                    if (speakers[value].sum > speakers[i].sum){
+                        errors_block.append(
+                            '<p>Сумма спикерских у ' + (value).toString() + ' команды не должна быть больше чем у ' + i.toString() + ' команды</p>'
+                        );
+                        checked_room = false;
+                    }
                 }
             });
         }
