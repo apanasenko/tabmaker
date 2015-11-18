5cd0606680c2939c5790c46ad689b174f8d170e4
Alexander Apanasenko <alexander.apanasenko@gmail.com>
2015-11-18 18:19:46 +1000

    Fix: не отчищал сообщения об ошибках при новых ajax запросах

--- apps/tournament/static/tournament/js/add_adjudicators.js
+++ apps/tournament/static/tournament/js/add_adjudicators.js
@@ -1,13 +1,14 @@
 function save_adjudicator(url){
-    var new_admin_block = $('#new_adjudicator');
-    var name_input = new_admin_block.find('#add_adjudicator_input');
+    var new_adjudicator_block = $('#new_adjudicator');
+    var name_input = new_adjudicator_block.find('#add_adjudicator_input');
+    new_adjudicator_block.find('.errors').html('');
     $.post(
         url,
         {
             email: name_input.val()
         },
         function(data, status){
-            new_admin_block.find('.errors').html(data.message);
+            new_adjudicator_block.find('.errors').html(data.message);
             if (data.status == 'ok'){
                 name_input.val('');
                 location.reload();
