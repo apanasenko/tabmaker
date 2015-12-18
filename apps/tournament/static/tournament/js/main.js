3f2af1002139d716f289cd0ae444306bad6a016c
Alexander Apanasenko <alexander.apanasenko@gmail.com>
2015-12-19 00:49:32 +1000

    Сообщения при управлении своими заявками

--- apps/tournament/static/tournament/js/main.js
+++ apps/tournament/static/tournament/js/main.js
@@ -1,25 +1,37 @@
-function show_message(data, div_id, errors_id){
-    if (data.status == 'ok') {
-        $('#' + div_id).html(data.message)
-    } else {
-        $('#' + errors_id).html(data.message)
+var error = 'notification-red';
+var success = 'notification-green';
+var message = null;
+
+
+function show_message(data, div_id, message_id){
+    if (message !== null){
+        message.removeClass();
+        message.html('');
     }
+
+    var css_class = error;
+    message = $('#' + message_id);
+    if (data.status == 'ok') {
+        $('#' + div_id).remove();
+        css_class = success;
+    }
+    message.attr('class', css_class).html(data.message);
 }
 
 
-function remove_team(url, id, div_team_id, div_errors_id){
+function remove_team(url, id, div_team_id, div_message_id){
     $.post(url, {team_id: id},
         function(data, status){
-            show_message(data, div_team_id, div_errors_id);
+            show_message(data, div_team_id, div_message_id);
         }
     );
 }
 
 
-function remove_adjudicator(url, id, div_adjudicator_id, div_errors_id){
+function remove_adjudicator(url, id, div_adjudicator_id, div_message_id){
     $.post(url, {user_tournament_rel_id: id},
         function(data, status){
-            show_message(data, div_adjudicator_id, div_errors_id);
+            show_message(data, div_adjudicator_id, div_message_id);
         }
     );
 }
