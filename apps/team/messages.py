a1a94dfbc13641699f0e60799c9fd1d1b67154a7
platonov <vitaliiplatonov@gmail.com>
2016-01-11 18:36:43 +1000

    страница с результатами испорта

--- apps/team/messages.py
+++ apps/team/messages.py
@@ -1,11 +1,11 @@
-MSG_TEAM_URL = "Ссылка на google-таблицу"
+MSG_TEAM_URL = "Ссылка на таблицу Google Drive"
 
-MSG_TEAM_NAME = 'Колонка google-таблицы с названиями команд'
+MSG_TEAM_NAME = 'Названия команд'
 
-MSG_S1_EMAIL = 'Колонка google-таблицы с e-mail первого спикера'
-MSG_S2_EMAIL = 'Колонка google-таблицы с e-mail второго спикера'
+MSG_S1_EMAIL = 'Имена первых спикеров'
+MSG_S2_EMAIL = 'E-mail первых спикеров'
 
-MSG_S1_NAME = 'Колонка google-таблицы c именем первого спикера'
-MSG_S2_NAME = 'Колонка google-таблицы с именем второго спикера'
+MSG_S1_NAME = 'Имена вторых спикеров'
+MSG_S2_NAME = 'E-mail вторых спикеров'
 
 MSG_CELL_NOT_FOUND_p = '%s не найдена в данной таблице'
