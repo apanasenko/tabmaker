cf5e0a0d72f5dc620306f6378ce00919da3957f6
Alexander Apanasenko <alexander.apanasenko@gmail.com>
2016-02-13 16:41:06 +1000

    Добавлен фильтр несостоявшихся турниров

--- apps/profile/views.py
+++ apps/profile/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import \
     get_object_or_404, \
     render
 from django.http import Http404
+from django.db.models import Count, Q
 from apps.tournament.consts import *
+from apps.tournament.models import Tournament
 from apps.main.utils import paging
 from . models import User
 from . forms import EditForm
@@ -50,15 +52,20 @@ def edit_profile(request, user_id):
 
 def show_tournaments_of_user(request, user_id):
     user = get_object_or_404(User, pk=user_id)
+
+    tournaments = Tournament.objects.annotate(
+        m_count=Count('team_members')
+    ).select_related('status').filter(
+        usertournamentrel__user=user, usertournamentrel__role__in=[ROLE_ADMIN, ROLE_OWNER]
+    ).order_by('-start_tour')
+
     return render(
         request,
         'main/main.html',
         {
+            'is_main_page': False,
             'is_owner': request.user == user,
-            'objects': paging(
-                request,
-                list(map(lambda x: x.tournament, user.usertournamentrel_set.filter(role=ROLE_OWNER)))
-            )
+            'objects': paging(request, tournaments)
         }
     )
 
