12d8f3990f76966a8d41cb0c872684e4e7a04dfe
Alexander Apanasenko <alexander.apanasenko@gmail.com>
2016-02-28 15:31:11 +1000

    Fix default static settings

--- DebatesTournament/settings/default/static.py
+++ DebatesTournament/settings/default/static.py
@@ -1,6 +1,6 @@
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = '<static_dir>'
 
 STATIC_URL = '/static/'
