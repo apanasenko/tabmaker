2deba889350006124ac9527dd5120efd71222a7f
Alexander Apanasenko <alexander.apanasenko@gmail.com>
2017-07-16 14:44:46 +1000

    Code style

--- apps/tournament/static/main/js/interface.js
+++ apps/tournament/static/main/js/interface.js
@@ -1,44 +1,35 @@
 // TODO настроить формат даты, который уходит из формы после включения js-пикера и включить пикеры
 
-
-
 // показать и скрыть баннер
-
 $(function() {
     $('.banner__close').click(function() {
         $('.banner').hide();
     });
 });
 
-
-
-
 // поведение табов
-
 $(document).ready(function () {
-  $('.accordion-tabs').each(function(index) {
-    $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
-  });
-  $('.accordion-tabs').on('click', 'li > a.tab-link', function(event) {
-    if (!$(this).hasClass('is-active')) {
-      event.preventDefault();
-      var accordionTabs = $(this).closest('.accordion-tabs');
-      accordionTabs.find('.is-open').removeClass('is-open').hide();
 
-      $(this).next().toggleClass('is-open').toggle();
-      accordionTabs.find('.is-active').removeClass('is-active');
-      $(this).addClass('is-active');
-    } else {
-      event.preventDefault();
-    }
-  });
+    $('.accordion-tabs').each(function(index) {
+        $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+    });
+
+    $('.accordion-tabs').on('click', 'li > a.tab-link', function(event) {
+        if (!$(this).hasClass('is-active')) {
+            event.preventDefault();
+            var accordionTabs = $(this).closest('.accordion-tabs');
+            accordionTabs.find('.is-open').removeClass('is-open').hide();
+
+            $(this).next().toggleClass('is-open').toggle();
+            accordionTabs.find('.is-active').removeClass('is-active');
+            $(this).addClass('is-active');
+        } else {
+            event.preventDefault();
+        }
+    });
 });
 
-
-
-
 // показ табов
-
 $(document).ready(function() {
     $('.menu-item').click(function () {
         var menuItem = $(this).find('.dropdown-content');
