9c841ca39dba08398148d5d872a0fdedb2be09a0
Alexander Apanasenko <alexander.apanasenko@gmail.com>
2017-07-29 20:37:13 +1000

    Fix: показывалась всегда первая вкладка

--- apps/tournament/static/main/js/interface.js
+++ apps/tournament/static/main/js/interface.js
@@ -11,20 +11,18 @@ $(function() {
 $(document).ready(function () {
 
     $('.accordion-tabs').each(function(index) {
-        $(this).children('li').first().children('a').addClass('is-active').next().addClass('is-open').show();
+        $(this).children('li').find('.is-active').next().addClass('is-open').show();
     });
 
     $('.accordion-tabs').on('click', 'li > a.tab-link', function(event) {
+        event.preventDefault();
+
         if (!$(this).hasClass('is-active')) {
-            event.preventDefault();
             var accordionTabs = $(this).closest('.accordion-tabs');
             accordionTabs.find('.is-open').removeClass('is-open').hide();
-
             $(this).next().toggleClass('is-open').toggle();
             accordionTabs.find('.is-active').removeClass('is-active');
             $(this).addClass('is-active');
-        } else {
-            event.preventDefault();
         }
     });
 });
