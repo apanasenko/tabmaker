1cdf8f8f901d465f07052ffbbc86f8303a44821e
Alexander Apanasenko <alexander.apanasenko@gmail.com>
2018-05-06 21:00:19 +1000

    Ref: заменил строковые название приложение на импорт в роутенге

--- DebatesTournament/urls.py
+++ DebatesTournament/urls.py
@@ -1,23 +1,25 @@
 from django.conf.urls import include, url
 from django.conf import settings
 from django.contrib import admin
-
+from apps.tournament.urls import main, account, tournament
 
 urlpatterns = [
-    url(r'^', include('apps.tournament.urls.main', namespace='main')),
+    url(r'^', include(main, namespace='main')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^profile/', include('apps.tournament.urls.account')),
-    url(r'^tournament/', include('apps.tournament.urls.tournament', namespace='tournament')),
+    url(r'^profile/', include(account)),
+    url(r'^tournament/', include(tournament, namespace='tournament')),
 ]
 
 if settings.TELEGRAM_BOT_ENABLE:
+    from django_telegrambot import urls as django_telegrambot_urls
     urlpatterns += [
-        url(r'^', include('django_telegrambot.urls')),
+        url(r'^', include(django_telegrambot_urls)),
     ]
 
 if settings.DEBUG:
     import debug_toolbar
+    from apps.tester import urls as tester_urls
     urlpatterns += [
-        url(r'', include('apps.tester.urls', namespace='tester')),
+        url(r'', include(tester_urls, namespace='tester')),
         url(r'^__debug__/', include(debug_toolbar.urls)),
     ]
