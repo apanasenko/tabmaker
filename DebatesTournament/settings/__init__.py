3df9b3130784ce3b900163bb62c033ec5003b674
Alexander Apanasenko <alexander.apanasenko@gmail.com>
2019-04-07 00:52:55 +1000

    Добавить инициализацию окружения из .env

--- DebatesTournament/settings/__init__.py
+++ DebatesTournament/settings/__init__.py
@@ -1,3 +1,9 @@
+import os
+if not int(os.getenv('ENVIRONMENT_LOADED', 0)):
+    # TODO Убрать после правильной настройки прода, пока так
+    import dotenv
+    dotenv.read_dotenv(os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), '.env'))
+
 from .defaults import *
 from .database import *
 from .allauth import *
