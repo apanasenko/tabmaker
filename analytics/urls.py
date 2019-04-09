2ff9c724dce29b6f336ec7b5d1b4085721dc8993
Pavel Mesenev <mesenev.pr@gmail.com>
2019-04-10 06:15:01 +1000

    minor clean-up

--- analytics/urls.py
+++ analytics/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.urls import path
 
-from analytics.views import index, MotionList, ProfileAPI
+from analytics.views import index, ProfileAPI
 
 app_name = "analytics"
 
